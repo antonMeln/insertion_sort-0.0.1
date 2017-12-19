499d38d7414fba0b3db6cb9d410ccd50402b5018
antonMeln <32076906+antonMeln@users.noreply.github.com>
2017-12-19 20:44:02 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+# insertion_sort-0.0.1
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool read (int & n, int * mas)
+{
+  string str;
+   getline(cin,str);
+    istringstream stream(str);
+     bool F = true;
+    
+       for (int i=0;i<n;i++)
+        {
+          if (!(stream >> mas[i])) { F = false; break;}
+        }
+   return F;
+}
+
+int main ()
+ { 
+   int * mas = new int; 
+   int i, n;
+   int min;
+   string str;
+   getline(cin,str);
+   istringstream  stream (str);
+    
+       if (stream >> n){
+         if (read(n,mas)){
+          
+          int a,j;
+          for (int i = 1;i<n;i++){
+            a= mas[i];
+            j=i-1;
+             while (mas[j]>a && j>=0){
+               mas[j+1]=mas[j];
+               mas[j]=a;
+               j--;
+               
+             }
+          }
+          for (int i=0;i <n; i++) cout << mas[i] << " ";
+          
+          
+} else cout << "An error has occured while reading input data.";
+       }      
+ return 0;
+ }  
