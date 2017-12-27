404d16c5431fde81b57b458f45667a774bb92c4c
antonMeln <32076906+antonMeln@users.noreply.github.com>
2017-12-27 18:14:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 bool read (int & n, int * mas)
 {
   string str;
-   getline(cin,str);
-    istringstream stream(str);
-     bool F = true;
+  getline(cin,str);
+  istringstream stream(str);
+  bool F = true;
     
-       for (int i=0;i<n;i++){
-          if (!(stream >> mas[i])) {
-                F = false;
-                break;
+  for (int i=0;i<n;i++){
+    if (!(stream >> mas[i])) {
+        F = false;
+        break;
              
-          }
-        }
+     }
+  }
    return F;
 }
 
 int main ()
  { 
-   int * mas = new int; 
    int i, n;
+   int * mas = new int[n];
    int min;
    string str;
    getline(cin,str);
@@ -32,22 +32,23 @@ int main ()
        if (stream >> n){
          if (read(n,mas)){
           
-          int a,j;
-          for (int i = 1;i<n;i++){
-            a= mas[i];
-            j=i-1;
-             while (mas[j]>a && j>=0){
-               mas[j+1]=mas[j];
-               mas[j]=a;
-               j--;
+            int a,j;
+            for (int i = 1;i<n;i++){
+              a= mas[i];
+              j=i-1;
+               while (mas[j]>a && j>=0){
+                mas[j+1]=mas[j];
+                mas[j]=a;
+                j--;
                
-             }
-          }
-          for (int i=0;i <n; i++) 
+              }
+            }
+            for (int i=0;i <n; i++) 
              cout << mas[i] << " ";
           
-          
-} else cout << "An error has occured while reading input data.";
-       }      
- return 0;
+          } 
+          else cout << "An error has occured while reading input data.";
+       }
+  delete[] mas;
+  return 0;
  }  
